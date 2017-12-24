316b41d9d7585e7d8347c7b35d997a6d8cacbba4
Phil Gates-Shannon <phil.gs@gmail.com>
2017-12-23 20:18:59 -0500

    Experiment with custom cookie

--- src/spike/routes.js
+++ src/spike/routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import ms from 'ms';
 
 const PARENT_ROUTE = '/spike';
 const router = express.Router();
@@ -16,10 +17,16 @@ router.get(
 router.get(
     '/login',
     ( request, response ) => {
-        request.session.user = {
+        response.cookie( 'user', JSON.stringify( {
             id: '99',
             name: 'super cool guy'
-        };
+        } ), {
+            encode: string => string,
+            httpOnly: true,
+            maxAge: ms( '90d' ),
+            sameSite: true,
+            secure: true
+        } );
         response.redirect( `${PARENT_ROUTE}/` );
     }
 );
@@ -27,10 +34,12 @@ router.get(
 router.get(
     '/logout',
     ( request, response ) => {
-        request.session.user = null;
-        delete request.session.user;
-        response.clearCookie( 'session-id' );
-        request.session.destroy( () => response.redirect( `${PARENT_ROUTE}/` ) );
+        // request.session.user = null;
+        // delete request.session.user;
+        // response.clearCookie( 'session-id' );
+        // request.session.destroy( () => response.redirect( `${PARENT_ROUTE}/` ) );
+        response.clearCookie( 'user' );
+        response.redirect( `${PARENT_ROUTE}/` );
     }
 );
 
