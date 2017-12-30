c7f2d65bda96feef538de7ec7814fd15bd2fad31
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-12-30 09:15:31 -0500

    Updates to experiment with Glados's "complete auth2" middleware function

--- src/glados/routes.js
+++ src/glados/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
-
 import GladosFactory from '@philgs/glados';
 
+const PARENT_ROUTE = '/glados';
 const glados = GladosFactory.create();
 const router = express.Router();
 
@@ -14,13 +14,15 @@ router.get(
 router.get(
     '/callback',
     glados.completeOAuth2(),
-    ( request, response ) => response.redirect( request.session.returnTo || '/user' )
+    ( request, response ) => response.redirect( request.session.returnTo || `${PARENT_ROUTE}/user` )
 );
 
 router.get(
     '/user',
-    glados.ensureAuthenticated(),
-    ( request, response ) => response.render( 'user-details.ejs' )
+    // glados.ensureAuthenticated(),
+    ( request, response ) => response.render( 'user-details.ejs', {
+        userObject: JSON.stringify( request.session.user, null, 4 )
+    } )
 );
 
 router.get(
