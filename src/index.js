813ab966895f2aaeeff7a4c59bc80ae5e1197b4f
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-12-31 07:36:29 -0500

    Remove cruft from Express Session and Passport in main app

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import bodyParser from 'body-parser';
 import flash from 'connect-flash';
 import cookieParser from 'cookie-parser';
 import express from 'express';
-import session from 'express-session';
-import fs from 'fs';
 import logger from 'morgan';
-import ms from 'ms';
-import passport from 'passport';
-import Auth0Strategy from 'passport-auth0';
 import path from 'path';
 
 import glados from '@philgs/glados';
 
-import * as login from './login/utils';
 import routes from './routes';
 
 const app = express();
@@ -37,32 +31,6 @@ glados.configureOAuth2( gladosOptions, app );
 glados.configureSessionStore( app.locals );
 
 
-// --- CONFIGURE PASSPORT TO USE AUTH0 ---
-
-const strategy = new Auth0Strategy(
-    {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        callbackURL: process.env.AUTH0_CALLBACK_URL
-    },
-    login.verifyAuth0IdToken
-);
-// noinspection JSCheckFunctionSignatures
-passport.use( strategy );
-
-// Write session data to a cookie
-passport.serializeUser( ( user, done ) => {
-    // TODO Control serialization and cookie
-    done(null, user);
-} );
-
-// Read session data from a cookie
-passport.deserializeUser( ( user, done ) => {
-    done(null, user);
-} );
-
-
 // --- CONFIGURE LOGGING ---
 
 if ( process.env.NODE_ENV === 'development' ) {
@@ -79,34 +47,9 @@ if ( process.env.NODE_ENV === 'production' ) {
     app.set( 'trust proxy', 1 );
 }
 
-// Session cookie options
-const sessionOptions = {
-    cookie: {
-        httpOnly: true,
-        maxAge: ms( '90d' ),
-        sameSite: true,
-        secure: true
-    },
-    genid: login.generateSessionId,
-    name: login.getSessionCookieName(),
-    resave: false,
-    saveUninitialized: false,
-    secret: login.getSessionKey(),
-    unset: 'destroy'
-};
-
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( cookieParser() );
-// TODO Secure session with options and `libsodium`
-// app.use( session( {
-//     secret: 'asdf09987',
-//     resave: true,
-//     saveUninitialized: true
-// } ) );
-// app.use( session( sessionOptions ) );
-// app.use( passport.initialize() );
-// app.use( passport.session() );
 app.use( glados.getSessionMiddleware() );
 app.use( flash() );
 app.use( '/static', express.static( path.resolve( appRoot, 'static' ) ) );
