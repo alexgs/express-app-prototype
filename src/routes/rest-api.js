5cc0fcb33ade9ed2dedb9fbe73b0ac378b75741b
Phil Gates-Shannon <phil.gs@gmail.com>
2018-02-07 09:23:04 -0500

    Add additional REST endpoints

--- src/routes/rest-api.js
+++ src/routes/rest-api.js
@@ -12,7 +12,6 @@ cluster.authenticate( username, password );
 const bucket = cluster.openBucket( bucketName );
 const N1qlQuery = couchbase.N1qlQuery;
 
-
 // GET all breweries
 router.get( '/breweries', function( request, response, next ) {
     const queryString = `
@@ -30,5 +29,32 @@ router.get( '/breweries', function( request, response, next ) {
     } );
 } );
 
+// GET a specific brewery
+router.get( '/breweries/:breweryId', function( request, response, next ) {
+    bucket.get( request.params[ 'breweryId' ], ( error, result, meta ) => {
+        if ( error ) {
+            response.send( { CouchbaseError: error } );
+        } else {
+            response.json( result.value );
+        }
+    } );
+} );
+
+// GET all beers from a specific brewery
+router.get( '/breweries/:breweryId/beers', function( request, response, next ) {
+    const queryString = `
+        SELECT META().id, name, category, style
+        FROM \`beer-sample\`
+        WHERE type = "beer" AND brewery_id = $1    
+    `;
+    const query = N1qlQuery.fromString( queryString );
+    bucket.query( query, [ request.params[ 'breweryId' ] ], ( error, rows, meta ) => {
+        if ( error ) {
+            response.send( { CouchbaseError: error } );
+        } else {
+            response.json( rows );
+        }
+    } );
+} );
 
 export default router;
